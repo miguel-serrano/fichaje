c7042b3e5a0f7bd9fe89180c48a419887757b32e
Miguel Serrano <miguelserranocabeza@gmail.com>
2026-01-22 19:15:01 +0100

    User is not agregate root of TimeEntry

--- app/DDD/TimeTracking/Application/Command/ClockOutCommand.php
+++ app/DDD/TimeTracking/Application/Command/ClockOutCommand.php
@@ -2,22 +2,19 @@
 
 namespace App\DDD\TimeTracking\Application\Command;
 
-use App\DDD\TimeTracking\Domain\ValueObjects\TimeEntryId;
 use App\DDD\User\Domain\ValueObjects\Uuid;
 
 final class ClockOutCommand
 {
     private function __construct(
         public readonly Uuid $userUuid,
-        public readonly ?TimeEntryId $timeEntryId,
     ) {
     }
 
-    public static function create(string $userUuid, ?int $timeEntryId = null): self
+    public static function create(string $userUuid): self
     {
         return new self(
             userUuid: new Uuid($userUuid),
-            timeEntryId: TimeEntryId::makeOrNull($timeEntryId),
         );
     }
 }
